a42f7a779bd403ba6404fcbbeac151759493ed2c
Felipe <felipeadesouza@gmail.com>
2018-01-18 18:03:54 -0200

    Segundo commit

---
+++ src/Aplicacao/Main.java
@@ -0,0 +1,17 @@
+package Aplicacao;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		List<String> lista = new ArrayList<>();
+		
+		List<String> lista2 = Arrays.asList("Felipe","Kamila","Madruga","Mel");
+	}
+
+}
---
+++ src/Modelo/Pessoa.java
@@ -0,0 +1,32 @@
+package Modelo;
+
+import java.time.LocalDate;
+
+public class Pessoa {
+
+	private String nome;
+	private LocalDate nascimento;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getNascimento() {
+		return nascimento;
+	}
+
+	public void setNascimento(LocalDate nascimento) {
+		this.nascimento = nascimento;
+	}
+
+	public Pessoa(String nome, LocalDate nascimento) {
+		super();
+		this.nome = nome;
+		this.nascimento = nascimento;
+	}
+
+}
